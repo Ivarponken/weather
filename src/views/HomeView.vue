7735ea952c357e8322531d03bfb7c013a918fbbf
Ivar <63834@gymnasium.ax>
2025-10-10 14:06:11 +0300

    resnat bort scaffolding-kod  och lagt in hämtning och transformering av väderdata

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,36 @@
 <script setup>
-import TheWelcome from '../components/TheWelcome.vue'
+import { onMounted, ref } from 'vue'
+
+const location = ref({ lat: 60.0, long: 20.0, name: 'Nuvarande position' })
+
+import { getForecast } from '@/services/forecastService'
+const info = ref({})
+onMounted(() => {
+  getForecast(location.value)
+    .then((response) => {
+      info.value = response
+    })
+    .catch((err) => {
+      console.log(err)
+    })
+})
 </script>
 
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+  <h2>{{ location.name }}</h2>
+  <p class="location">
+    Lat: <span> {{ location.lat.toFixed(3) }} </span>
+  </p>
+  <p class="location">
+    Long: <span> {{ location.long.toFixed(3) }} </span>
+  </p>
+  <div>
+    {{ info }}
+  </div>
 </template>
+<style scoped>
+.location {
+  display: inline-block;
+  margin: 0 1em;
+}
+</style>
